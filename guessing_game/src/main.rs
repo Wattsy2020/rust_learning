67f90ae846f5b8df0aaeab32b3d0cab98a8f3d84
Liam Watts <liam.watts271@gmail.com>
2024-11-03 21:44:11 +1100

    working game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,46 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
+fn read_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input
+}
+
+fn gen_random_number() -> u32 {
+    rand::thread_rng().gen_range(1..=100)
+}
 
 fn main() -> () {
     println!("Guess the number!");
-    println!("Please input your guess: ");
+    let rand_number = gen_random_number();
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess: ");
+        let guess = read_input();
+        let trimmed = guess.trim();
+        if trimmed == "exit" {
+            break;
+        }
+        let guess_num: u32 = match trimmed.parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Error: {err}, {trimmed} is not a number");
+                continue;
+            }
+        };
+        println!("You guessed: {guess_num}");
 
-    println!("You guessed: {}", guess);
+        match guess_num.cmp(&rand_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
